171b56057983c7ab26bcbac72c3a2b1d380bbf0e
Matheus Vancoll <matheu-s_andre@hotmail.com>
2021-03-17 23:34:23 -0300

    update function auto login

--- sisfies-api/sisfies-api.js
+++ sisfies-api/sisfies-api.js
@@ -1,10 +1,14 @@
-
 const cpfRF = "70636151492"
 const pwRF = "inaa9121"
 
 function getUser(unid){
     if(unid == "RF"){
-        alert(cpfRF)
         return cpfRF
+    }
+}
+
+function getPassword(uni){
+    if(uni == "RF"){
+        return pwRF
     }
 }
--- sisfies-shortcut.js
+++ sisfies-shortcut.js
@@ -7,22 +7,38 @@ function validateLogin(){
 const campoSistema = document.querySelector("#nomeDoSistema")
 
 const btnRF = document.createElement("button")
+const btnSF = document.createElement("button")
+const btnNC = document.createElement("button")
+const btnJM = document.createElement("button")
+const btnMOS = document.createElement("button")
 
 startShortcuts()
 
 function startShortcuts(){
-    insertBtnLogin(btnRF, "RF", "RF")
+
+    insertBtnLogin(btnRF, "RF")
+    //insertBtnLogin(btnSF, "SF")
+    //insertBtnLogin(btnNC, "NC")
+    //insertBtnLogin(btnJM, "JM")
+    //insertBtnLogin(btnRF, "MOS")
     
-    function insertBtnLogin(btnName, viewName){
+    function insertBtnLogin(btnName, viewName){ // insere os botoes
         btnName.innerHTML = viewName
         btnName.classList.add("btnInput")
         campoSistema.appendChild(btnName)
         btnName.addEventListener("click",()=>{
-            const param = "RF"
-            const teste = getUser(param)
-            alert("O segredo é " +  teste)
-            ///////////
+            const user = getUser(viewName)
+            const password = getPassword(viewName)
+            loginUnid(user, password)
         })
+    }
+
+    function loginUnid(user, password){
+        const cpf = document.querySelector("#id")
+        const senha = document.querySelector("#pw")
+        const enterLogin = document.querySelector("#botoes")
+        cpf.value = user.value
+        senha.value = password.value
     }
 }
 	
